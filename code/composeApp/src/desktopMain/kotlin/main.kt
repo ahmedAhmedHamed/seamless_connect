c8916d9b9c607a73a7e8b922a1b388f691e143ff
Ahmed Hatem <ahmehatem32@gmail.com>
2024-06-23 20:55:55 -0700

    I created an instance of the ServerSocket class that accept client request

--- code/composeApp/src/desktopMain/kotlin/main.kt
+++ code/composeApp/src/desktopMain/kotlin/main.kt
@@ -1,11 +1,32 @@
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
+import java.net.ServerSocket
+import java.io.IOException
+import java.net.BindException
+import java.net.SocketException
+import java.io.DataInputStream
 
-fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "code",
-    ) {
-        App()
+fun main() {
+    val portNum = 8181
+    try {
+        //Create instance of "ServerSocket" class with portNumber
+        val primarySockett = ServerSocket(portNum)
+
+        // Block the execution of the program until a client connects to the server on the port that firstSockett is listening to.
+        val finalSockett = primarySockett.accept()
+
+            // Testing if the sockets are successfully created using DataInputStream
+        val dis = DataInputStream(finalSockett.getInputStream())
+        val str = dis.readUTF()
+        println("Message = " + str)
+
+        primarySockett.close()
+        finalSockett.close()
+    }catch (e: IOException){
+        println("IOException occurred: ${e.message}")
+    }catch (e: SocketException){
+        println("SocketException occurred: ${e.message}")
+    }catch (e: BindException){
+        println("BindException occurred: ${e.message}")
+    }catch (e: SecurityException){
+        println("SecurityException occurred: ${e.message}")
     }
 }
