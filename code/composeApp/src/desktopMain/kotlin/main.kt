3e98b5f77df7595ff27f8b04d9f3368abcb438b9
Ahmed Hatem <ahmehatem32@gmail.com>
2024-06-26 13:19:30 -0700

    Enhanced the code readability
    Before this commit I used the main function as a test file to learn more about what should/can I do. Now I returned the original main file and created a new file.

--- code/composeApp/src/desktopMain/kotlin/main.kt
+++ code/composeApp/src/desktopMain/kotlin/main.kt
@@ -1,32 +1,13 @@
-import java.net.ServerSocket
-import java.io.IOException
-import java.net.BindException
-import java.net.SocketException
-import java.io.DataInputStream
+import DeviceConnector.connectToDevice
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
 
-fun main() {
-    val portNum = 8181
-    try {
-        //Create instance of "ServerSocket" class with portNumber
-        val primarySockett = ServerSocket(portNum)
-
-        // Block the execution of the program until a client connects to the server on the port that firstSockett is listening to.
-        val finalSockett = primarySockett.accept()
-
-            // Testing if the sockets are successfully created using DataInputStream
-        val dis = DataInputStream(finalSockett.getInputStream())
-        val str = dis.readUTF()
-        println("Message = " + str)
-
-        primarySockett.close()
-        finalSockett.close()
-    }catch (e: IOException){
-        println("IOException occurred: ${e.message}")
-    }catch (e: SocketException){
-        println("SocketException occurred: ${e.message}")
-    }catch (e: BindException){
-        println("BindException occurred: ${e.message}")
-    }catch (e: SecurityException){
-        println("SecurityException occurred: ${e.message}")
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "code",
+    ) {
+        connectToDevice("192.168.1.30")
+        App()
     }
 }
