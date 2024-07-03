29cc5b4ea2a175046324c6c20f58acaccd78f1dc
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2024-07-03 16:17:18 +0300

    changed styling of wakeuponlancard

--- code/composeApp/src/commonMain/kotlin/gui/wakeUpOnLan/wakeUpOnLanCard.kt
+++ code/composeApp/src/commonMain/kotlin/gui/wakeUpOnLan/wakeUpOnLanCard.kt
@@ -1,13 +1,25 @@
 package gui.wakeUpOnLan
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
-fun wakeUpOnLanCard(ipAddress: String, MAC: String, portNumber: String) {
-    Column {
-        Text(text = ipAddress)
-        Text(text = MAC)
-        Text(text = portNumber)
+fun wakeUpOnLanCard(ipAddress: String, MAC: String, portNumber: Int = 9) {
+    Surface (modifier = Modifier.fillMaxWidth(),
+        color = MaterialTheme.colors.primary) {
+        Column(modifier = Modifier.clickable {
+            println(ipAddress)
+            println(MAC)
+        }) {
+            Text(text = ipAddress)
+            Text(text = MAC)
+            Text(text = portNumber.toString())
+        }
     }
+
 }
