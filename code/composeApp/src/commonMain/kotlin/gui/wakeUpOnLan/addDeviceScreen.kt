840d618d22f21c8e87d145461042bbfe63435d4d
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2024-07-08 03:37:48 +0300

    better mac address field

--- code/composeApp/src/commonMain/kotlin/gui/wakeUpOnLan/addDeviceScreen.kt
+++ code/composeApp/src/commonMain/kotlin/gui/wakeUpOnLan/addDeviceScreen.kt
@@ -3,7 +3,10 @@ package gui.wakeUpOnLan
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -12,10 +15,44 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextRange
-import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.TextFieldValue
-import java.util.Locale
+
+@Composable
+fun TextFieldRow() {
+    val textFields = remember { mutableStateListOf("", "", "", "","", "", "", "") } // Initial empty list of 8 text fields
+    val focusManager = LocalFocusManager.current
+    val focusRequesters = remember { List(textFields.size) { FocusRequester() } }
+
+
+    Row (modifier = Modifier.fillMaxWidth()) {
+        textFields.forEachIndexed { index, text ->
+            BasicTextField(
+                value = text,
+
+                onValueChange = { newText ->
+                    if (newText.length <= 2)
+                        textFields[index] = newText.uppercase()
+                    if (newText.length >= 2 && index < textFields.size - 1) {
+                        focusManager.moveFocus(FocusDirection.Next)
+                    }
+                },
+                modifier = Modifier.weight(1f)
+                    .focusRequester(focusRequesters[index]),
+//                textStyle = TextStyle(fontSize = 9.sp)
+            )
+            if (index < textFields.size - 1) {
+                Text(
+                    text = ":", // Customize this text as needed
+//                    style = TextStyle(fontSize = 9.sp)
+                )
+            }
+        }
+    }
+}
 
 
 @Composable
@@ -23,86 +60,77 @@ fun addDevicePrompt() {
     var text1 by remember { mutableStateOf(TextFieldValue("")) }
     var text2 by remember { mutableStateOf("") }
     var text3 by remember { mutableStateOf(TextFieldValue("")) }
+    AlertDialog(
+        onDismissRequest = {  },
+        title = { Text("Add Device") },
+        text = {
+            Column {
+                TextField(
+                    value = text1,
+                    onValueChange = { text1 = it },
+                    label = { Text("Device Name.") }
+                )
 
-        Column {
-//                TextField(
-//                    value = text1,
-//                    onValueChange = { text1 = it },
-//                    label = { Text("Device Name.") }
-//                )
-//
-//                TextField(
-//                    value = text2,
-//                    onValueChange = {
-//                        text2 = it
-//                            },
-//                    label = { Text("IPAddress.") }
-//                )
+                TextField(
+                    value = text2,
+                    onValueChange = {
+                        text2 = it
+                    },
+                    label = { Text("IPAddress.") }
+                )
 
-            TextFieldRow()
+                TextFieldRow()
 
-//                MacAddressInput(
-//                    value = text3,
-//                    onValueChange = { text3 = it },
-//                    label = { Text("MAC address.") }
-//                )
-        }
-}
-
-@Composable
-fun TextFieldRow() {
-    val textFields = remember { mutableStateListOf("", "", "", "", "", "", "", "") } // Initial empty list of 8 text fields
-
-    Row (modifier = Modifier.fillMaxWidth()) {
-        textFields.forEachIndexed { index, text ->
-            TextField(
-                value = text,
-                onValueChange = { newText ->
-                    textFields[index] = newText
-                },
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    capitalization = KeyboardCapitalization.Characters
-                ),
-                modifier = Modifier.weight(1f)
-            )
-        }
-    }
-}
-
-@Composable
-fun MacAddressInput(
-    value: TextFieldValue,
-    onValueChange: (TextFieldValue) -> Unit,
-    label: @Composable () -> Unit
-) {
-    TextField(
-        value = value,
-        onValueChange = {
-            val oldSelectionIdx = it.selection.min
-            val newValue = formatMacAddress(it.text, oldSelectionIdx)
-            val selectionIdx: Int = if (it.text.length < newValue.length)
-                oldSelectionIdx + 1
-            else
-                oldSelectionIdx
-
-            val selection = TextRange(selectionIdx)
-            onValueChange(TextFieldValue(text = newValue, selection = selection))
+            }
         },
-        label = label,
+        confirmButton = {
+            Button(
+                onClick = {
+                    println("Text 1: ${text1.text}")
+                    println("Text 2: ${text2}")
+                    println("Text 3: ${text3.text}")
+                }
+            )
+            { Text("Submit") }
+        }
     )
 }
 
-fun formatMacAddress(input: String, selectionIdx: Int): String {
-    val cleanInput = input.replace(":", "").replace("-", "").uppercase(Locale.getDefault())
-    val formatted = buildString {
-        for (i in cleanInput.indices) {
-            if (i > 0 && i % 2 == 0) append(':')
-            append(cleanInput[i])
-        }
-        if (this.count() > 17)  deleteCharAt(selectionIdx - 1)
-        while (this.count() > 17) deleteCharAt(this.count() - 1)
-    }
 
-    return formatted
-}
+//@Composable
+//fun MacAddressInput(
+//    value: TextFieldValue,
+//    onValueChange: (TextFieldValue) -> Unit,
+//    label: @Composable () -> Unit
+//) {
+//    TextField(
+//        value = value,
+//        onValueChange = {
+//            val oldSelectionIdx = it.selection.min
+//            val newValue = formatMacAddress(it.text, oldSelectionIdx)
+//            val selectionIdx: Int = if (it.text.length < newValue.length)
+//                oldSelectionIdx + 1
+//            else
+//                oldSelectionIdx
+//
+//            val selection = TextRange(selectionIdx)
+//            onValueChange(TextFieldValue(text = newValue, selection = selection))
+//        },
+//        label = label,
+//    )
+//}
+//
+//fun formatMacAddress(input: String, selectionIdx: Int): String {
+//    val cleanInput = input.replace(":", "").replace("-", "").uppercase(Locale.getDefault())
+//    val formatted = buildString {
+//        for (i in cleanInput.indices) {
+//            if (i > 0 && i % 2 == 0) append(':')
+//            append(cleanInput[i])
+//        }
+//        if (this.count() > 17)  deleteCharAt(selectionIdx - 1)
+//        while (this.count() > 17) deleteCharAt(this.count() - 1)
+//    }
+//
+//    return formatted
+//}
 
