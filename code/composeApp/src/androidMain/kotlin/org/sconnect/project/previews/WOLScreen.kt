c8fc0bd02a99452b77820f2e79ee033cbaf82416
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2024-07-14 23:21:10 +0300

    Create WOLScreen.kt

---
+++ code/composeApp/src/androidMain/kotlin/org/sconnect/project/previews/WOLScreen.kt
@@ -0,0 +1,11 @@
+package org.sconnect.project.previews
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import gui.wakeUpOnLan.wakeUpOnLanScreen
+
+@Preview
+@Composable
+fun WakeUpOnLanScreen() {
+    wakeUpOnLanScreen()
+}
