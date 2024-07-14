44c7d3318106003e9c50730dcf9f216a15040418
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2024-07-14 23:21:03 +0300

    Update wakeUpOnLanCardPreview.kt

--- code/composeApp/src/androidMain/kotlin/org/sconnect/project/previews/wakeUpOnLanCardPreview.kt
+++ code/composeApp/src/androidMain/kotlin/org/sconnect/project/previews/wakeUpOnLanCardPreview.kt
@@ -6,6 +6,6 @@ import gui.wakeUpOnLan.WakeUpOnLanCard
 @Preview
 @Composable
 fun WakeUpOnLanCardPreview() {
-    WakeUpOnLanCard(ipAddress = "192.168.1.3", MAC = "00-B0-D0-63-C2-26", portNumber = 9).show()
+    WakeUpOnLanCard("fat7y", ipAddress = "192.168.1.3", MAC = "00-B0-D0-63-C2-26", portNumber = 9)
 }
 
