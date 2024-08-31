e8de6cf1594f5564a646f9185559d92161899d2a
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2024-08-31 17:15:49 +0300

    renamed client socket connection function

--- v2/code/composeApp/src/commonMain/kotlin/Connections/socket/Client.kt
+++ v2/code/composeApp/src/commonMain/kotlin/Connections/socket/Client.kt
@@ -1,17 +1,16 @@
 package connections.socket
 import java.net.Socket
 
-fun connectToServer(serverIp: String, port: Int) : Boolean {
+fun createClientSocketConnection(serverIp: String, port: Int) : Socket? {
     println("trying to connect...")
     return try {
         val socket = Socket(serverIp, port)
-        val outputStream = socket.getOutputStream()
-        outputStream.write(5)
-        socket.close()
-        true
+//        val outputStream = socket.getOutputStream()
+//        outputStream.write(5)
+//        socket.close()
+        socket
     } catch (e: Exception) {
         println("connectToDevice has thrown an exception. $e")
-
-        false
+        null
     }
 }
