613772baf6c7d622ee3dcddc53fb7d69cc5d8ae6
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2024-09-16 20:02:09 +0300

    removed otg_mode_check from instance variables in MainScreen

--- v5/code/main.py
+++ v5/code/main.py
@@ -2,27 +2,23 @@ import tkinter as tk
 from components import ScrollableButtonBox
 from engine import alaunch_scrcpy
 
+
 class MainScreen:
 
     def __init__(self):
-        self.device_selection_box: ScrollableButtonBox or None = None
-        self.scrcpy_result = None
         self.root = tk.Tk()
         self.root.title("Seamless Connect")
         self.build_phone_controls()
-        self.build_devices_list()
-
-    def build_devices_list(self):
         self.device_selection_box = ScrollableButtonBox(self.root)
+        self.otg_button_check = tk.BooleanVar()
 
     def build_phone_controls(self):
         self.launch_bridge_mirroring_btn = tk.Button(self.root, text="launch bridge mirroring", command=self.launch_scrcpy,
                                                      font=("Arial", 12))
         self.launch_bridge_mirroring_btn.pack(pady=10)
 
-        self.otg_button_check = tk.BooleanVar()
-        self.otg_mode_check = tk.Checkbutton(self.root, text="OTG mode", variable=self.otg_button_check)
-        self.otg_mode_check.pack(pady=10)
+        otg_mode_check = tk.Checkbutton(self.root, text="OTG mode", variable=self.otg_button_check)
+        otg_mode_check.pack(pady=10)
 
     def get_scrcpy_flags(self):
         command = []
