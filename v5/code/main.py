a8eec630cd31ef217e4bc34ec6d6f941c2ecf530
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2024-09-16 15:02:54 +0300

    improved variable names in main

--- v5/code/main.py
+++ v5/code/main.py
@@ -8,7 +8,7 @@ from components import ScrollableButtonBox
 class MainScreen:
 
     def __init__(self):
-        self.device_selection: ScrollableButtonBox or None = None
+        self.device_selection_box: ScrollableButtonBox or None = None
         self.scrcpy_result = None
         self.root = tk.Tk()
         self.root.title("Seamless Connect")
@@ -16,38 +16,35 @@ class MainScreen:
         self.build_devices_list()
 
     def build_devices_list(self):
-        self.device_selection = ScrollableButtonBox(self.root)
+        self.device_selection_box = ScrollableButtonBox(self.root)
 
     def build_phone_controls(self):
-        self.button = tk.Button(self.root, text="launch bridge mirroring", command=self.launch_scrcpy,
-                                font=("Arial", 12))
-        self.button.pack(pady=10)
+        self.launch_bridge_mirroring_btn = tk.Button(self.root, text="launch bridge mirroring", command=self.launch_scrcpy,
+                                                     font=("Arial", 12))
+        self.launch_bridge_mirroring_btn.pack(pady=10)
 
         self.otg_button_check = tk.BooleanVar()
-        self.button2 = tk.Checkbutton(self.root, text="OTG mode", variable=self.otg_button_check)
-        self.button2.pack(pady=10)
-
-    def update_label(self):
-        self.label.config(text="Button Clicked!")
+        self.otg_mode_check = tk.Checkbutton(self.root, text="OTG mode", variable=self.otg_button_check)
+        self.otg_mode_check.pack(pady=10)
 
     def get_scrcpy_command(self):
         command = ["dependencies/scrcpy-win64-v2.6.1/scrcpy.exe"]
         if self.otg_button_check.get():
             command.append("--otg")
-        if self.device_selection.get_selected_device_info()[0]:
-            device_serial, _ = self.device_selection.get_selected_device_info()
+        if self.device_selection_box.get_selected_device_info()[0]:
+            device_serial, _ = self.device_selection_box.get_selected_device_info()
             command.append(f'--serial={device_serial}')
         return command
 
     def non_blocking_scrcpy(self):
         scrcpy_process = subprocess.Popen(self.get_scrcpy_command(), stdout=sys.stdout)
         self.scrcpy_result = scrcpy_process.wait()
-        self.button.config(state=tk.NORMAL)
+        self.launch_bridge_mirroring_btn.config(state=tk.NORMAL)
 
     def launch_scrcpy(self):
-        self.button.config(state=tk.DISABLED)
-        task_thread = Thread(target=self.non_blocking_scrcpy)
-        task_thread.start()
+        self.launch_bridge_mirroring_btn.config(state=tk.DISABLED)
+        bridge_mirroring_thread = Thread(target=self.non_blocking_scrcpy)
+        bridge_mirroring_thread.start()
 
     def launch(self):
         self.root.mainloop()
